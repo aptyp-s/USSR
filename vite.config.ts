acf8db81222a52953962a33077fe937c72b7fc5f
Artur <arthur.scherbin@gmail.com>
2025-11-28 14:37:06 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,7 @@ export default defineConfig({
   base: '/USSR/', 
 
   plugins: [react()],
-  
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'), // Проверь, что папка src существует
-    }
-  },
-
-  // Это для локальной разработки, можно оставить
+    // Это для локальной разработки, можно оставить
   server: {
     port: 3000,
     host: '0.0.0.0',
